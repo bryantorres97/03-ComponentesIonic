12625ca20e57d8f81edf46f4d952cad6d69448d2
MrBlueSky <bryantorresckp@gmail.com>
2019-09-23 09:57:10 -0500

    Action sheet

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
+  }
+
+// SECTION Creación del action sheet
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums', // NOTE Es el nombre del action sheet
+      backdropDismiss: false, // NOTE Si está en falso, no permite cerrar el action si toca fuera de él
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive', // NOTE muestra la opción en rojo en ios
+        cssClass: 'rojo', // NOTE Crea una clase personalizada para mostrar en rojo en Android
+        icon: 'trash',
+        handler: () => { // NOTE  es la función que ejecuta al hacer click
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
