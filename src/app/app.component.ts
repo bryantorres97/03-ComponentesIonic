3308919c2176b0a90b1a37459429e360d2c5dc1f
MrBlueSky <bryantorresckp@gmail.com>
2019-10-04 18:06:25 -0500

    Split Pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/componente.interface';
+import { UsuariosService } from './providers/usuarios.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +13,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  componentes: Observable<Componente[]>;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: UsuariosService
   ) {
     this.initializeApp();
   }
@@ -22,6 +27,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.componentes = this.dataService.getMenuOptions();
     });
   }
 }
